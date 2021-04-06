9231f1578f8f61c1fb8e3a96b7a7d04ae7cb8875
Vinod Kumar Eraganaboina <Vinod-E>
2021-04-06 18:30:53 +0530

    E2E flow changes

--- Config/outputFile.py
+++ Config/outputFile.py
@@ -1,9 +1,8 @@
-import os
+from Config import CongfigFile
 
-PATH = os.getenv("HOME")
-GENERIC_INPUT_PATH = "%s/PythonFrameWorkNew/Pytest_UI/Reports/" % PATH
+GENERIC_OUTPUT_PATH = CongfigFile.OUTPUT_FILE
 
 OUTPUT_PATH = {
-    'E2E_output': GENERIC_INPUT_PATH + 'UI_CRPO_E2E_REGRESSION.xls',
-    'Mass_Interview_output': GENERIC_INPUT_PATH + 'UI_MASS_INTERVIEW.xls'
+    'E2E_output': GENERIC_OUTPUT_PATH + 'UI_CRPO_E2E_REGRESSION.xls',
+    'Mass_Interview_output': GENERIC_OUTPUT_PATH + 'UI_MASS_INTERVIEW.xls'
 }
